900bead388806726cbb645797e43f49ede949f3e
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 15:59:49 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,19 +1,22 @@
-if (loginSuccessful) {
-logger.severe(”User login succeeded for: ” + username);
-} else {
-logger.severe(”User login failed for: ” + username);
-}
-
-
-
-//Correct the code as shown in the Compliant Solution below:
+//Rule 00. Input Validation and Data Sanitization (IDS)
+//IDS03-J. Do not log unsanitized user input Given the non-compliant code below:
 
 if(loginSuccessful){
-logger.severe(”User login succeeded for: ” + sanitizeUser(username));
-}else{
-logger.severe(”User login failed for: ” + sanitizeUser(username));
+logger.severe(”User login succeeded for: ” + username);
+} else {
+logger.severe(”User login failed for: ”+username);
 }
-public String sanitizeUser(Stringusername){
-return Pattern.matches(”[A−Za−z0−9]+”,username))
-?username : ”unauthorizeduser”;
+//Correct the code as shown in theCompliant Solution below:
+
+if(loginSuccessful){
+logger.severe(”Userloginsucceededfor:”+sanitizeUser(username));
 }
+else{
+logger.severe(”Userloginfailedfor:”+sanitizeUser(username));
+}
+
+public String sanitizeUser(String username){
+return Pattern.matches(”[A−Za−z0−9_]+” , username))
+? username : ” unauthorized user”;
+}
+//Refer to: Rule 00. Input Validation and Data Sanitization (IDS)
