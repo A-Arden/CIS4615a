944e6d1a32d4424ffcaadd36b96e95cdc65304c2
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 15:28:27 -0400

    Create R00_IDS03_J.java

---
+++ R00_IDS03_J.java
@@ -0,0 +1,19 @@
+if (loginSuccessful) {
+logger.severe(”User login succeeded for: ” + username);
+} else {
+logger.severe(”User login failed for: ” + username);
+}
+
+
+
+//Correct the code as shown in the Compliant Solution below:
+
+if(loginSuccessful){
+logger.severe(”User login succeeded for: ” + sanitizeUser(username));
+}else{
+logger.severe(”User login failed for: ” + sanitizeUser(username));
+}
+public String sanitizeUser(Stringusername){
+return Pattern.matches(”[A−Za−z0−9]+”,username))
+?username : ”unauthorizeduser”;
+}
