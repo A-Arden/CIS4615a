79236988c77a2a69618062c08131119269a7f5a7
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 19:42:39 -0400

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -2,6 +2,7 @@
   Allison Arden
   3. Rule 03. Numeric Types and Operations (NUM)
 NUM03-J. Use integer types that can fully represent the possible range of unsigned data
+non-compliant code
 */
 
 public static int getInteger(DataInputStream is) throws IOException {
