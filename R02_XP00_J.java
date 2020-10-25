b3f0c1ee2a6ab85aae6e77094e10cf88de26d8bc
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 18:40:25 -0400

    Create R02_XP00_J.java

---
+++ R02_XP00_J.java
@@ -0,0 +1,12 @@
+/*
+  Allison Arden
+  2. Rule 02. Expressions (EXP)
+XP00-J. Do not ignore values returned by methods 
+*/
+public void deleteFile(){
+ 
+  File someFile = new File("someFileName.txt");
+  // Do something with someFile
+  someFile.delete();
+ 
+}
