1a59c0eb45b6553494523eedd6051e761c7af5b5
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 18:53:13 -0400

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -7,6 +7,8 @@ public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  someFile.delete();
+  if(!someFile.delete()){
+    //handle failure to delete the file
+  }
  
 }
