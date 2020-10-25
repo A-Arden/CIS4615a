b378db1f07d100ab0aa540616d48e59503d6d0b3
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 19:41:44 -0400

    Update R01_DCL00_J.java

--- R01_DCL00_J.java
+++ R01_DCL00_J.java
@@ -2,5 +2,20 @@
   Allison Arden
   6. Rule 01. Declarations and Initializations(DCL)
   DCL00-J. Prevent class initialization cycles
+   non-compliant code
 */
 
+
+public class Cycle {
+  private final int balance;
+  private static final Cycle c = new Cycle();
+  private static final int deposit = (int) (Math.random() * 100); // Random deposit
+ 
+  public Cycle() {
+    balance = deposit - 10; // Subtract processing fee
+  }
+ 
+  public static void main(String[] args) {
+    System.out.println("The account balance is: " + c.balance);
+  }
+}
