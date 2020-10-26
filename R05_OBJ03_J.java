fb562cb92124fd3558abdfcdc64015c3ba771cf9
A-Arden <73439234+A-Arden@users.noreply.github.com>
2020-10-25 20:11:58 -0400

    Update R05_OBJ03_J.java

--- R05_OBJ03_J.java
+++ R05_OBJ03_J.java
@@ -1,1 +1,18 @@
+/*
+  Allison Arden
+  7. Object Orientation (OBJ)
+  OBJ03-J Prevent heap pollution
+  compliant code
+*/
 
+class ListUtility {
+  private static void addToList(List<String> list, String str) {
+    list.add(str); // no warning generated
+  }
+ 
+  public static void main(String[] args) {
+    List<String> list = new ArrayList<String> ();
+    addToList(list, 42);
+    System.out.println(list.get(0)); 
+  }
+}
